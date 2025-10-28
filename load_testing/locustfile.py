e97cbd4dfc0a763e63fd62737624dedd6ae1346d
Rezaei_79 <62241326+abbasalirezaei@users.noreply.github.com>
2025-10-28 19:46:17 +0330

    added on_start method to login once

--- load_testing/locustfile.py
+++ load_testing/locustfile.py
@@ -5,8 +5,7 @@ class LoginUser(HttpUser):
     wait_time = between(1, 3)
     host = "http://web:8000"
 
-    @task
-    def login(self):
+    def on_start(self):
         response = self.client.post("/api/accounts/v1/auth/login/", json={
             "username": "test_user",
             "password": "test_password"
@@ -15,11 +14,28 @@ class LoginUser(HttpUser):
         if response.status_code == 200:
             token = response.json().get("access")
             self.headers = {"Authorization": f"Bearer {token}"}
-            print("✅ Login successful")
         else:
             self.headers = {}
-            print(f"❌ Login failed ({response.status_code}): {response.text}")
+
+    """
+        we don't need this task right now because we add on_start method to login once
+        @task
+        def login(self):
+            response = self.client.post("/api/accounts/v1/auth/login/", json={
+                "username": "test_user",
+                "password": "test_password"
+            })
+
+            if response.status_code == 200:
+                token = response.json().get("access")
+                self.headers = {"Authorization": f"Bearer {token}"}
+                print("Login successful")
+            else:
+                self.headers = {}
+                print(f"Login failed ({response.status_code}): {response.text}")
+
+    """
 
     @task
-    def get_website(self):
+    def get_websites(self):
         self.client.get('/api/accounts/v1/websites/', headers=self.headers)
